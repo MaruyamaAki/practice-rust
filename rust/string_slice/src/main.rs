90dc148bbc3e212db3b0d53be445e7b6c342b527
MaruyamaAki <maruyama@engineerforce.io>
2024-03-02 20:30:08 +0900

    studied string slices and string types.

--- rust/string_slice/src/main.rs
+++ rust/string_slice/src/main.rs
@@ -1,10 +1,27 @@
 fn main() {
-    let string1 = "string literal"; // &str　13byte
-    let string2 = "hello"; // &str 5byte
+    // let string1 = "string literal"; // &str　13byte
+    // let string2 = "hello"; // &str 5byte
+
+    // println!("Stack address of string1 is: {:p}", &string1);
+    // println!("Stack address of string2 is: {:p}", &string2);
+
+    // println!("Static memory address of string1: {:?}", string1.as_ptr());
+    // println!("Len of string1: {}", string1.len());
+
+    // let mut string1 = String::from("hello");
+    // let mut string2 = String::from("helloWorld");
+
+    let mut string1 = String::from("hello");
 
     println!("Stack address of string1 is: {:p}", &string1);
-    println!("Stack address of string2 is: {:p}", &string2);
-
     println!("Static memory address of string1: {:?}", string1.as_ptr());
     println!("Len of string1: {}", string1.len());
+    println!("Capacity of string1 is: {}", string1.capacity());
+
+    string1.push_str("new_value");
+
+    println!("Stack address of string1 is: {:p}", &string1);
+    println!("Static memory address of string1: {:?}", string1.as_ptr());
+    println!("Len of string1: {}", string1.len());
+    println!("Capacity of string1 is: {}", string1.capacity());
 }
