72659db76ff1065bbf4acb150cd86837c9ea7b8b
MaruyamaAki <maruyama@engineerforce.io>
2024-03-02 14:47:14 +0900

    stack領域でメモリが確保されているのを確認

--- rust/mutable_immutable/src/main.rs
+++ rust/mutable_immutable/src/main.rs
@@ -1,3 +1,23 @@
+const _MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    // let _x = 5;
+    
+    // // Rustの変数は全てImmutable
+    // // x = 6;
+
+    // let mut y = 5;
+    // println!("{}", y);
+    // // Mutableのmutを付けることでMutableになる
+    
+    // y = 6;
+    // println!("{}", y);
+
+    println!("System size: {}", usize::BITS); // System size: 64
+    println!("Memory address of const is: {:p}", &_MAX_POINTS); // Memory address of const is: 0xaaaaabb2d014
+
+    let integer1: i64 = 1;
+    let integer2: i64 = 2;
+    println!("Stack address of integer1 is: {:p}", &integer1);
+    println!("Stack address of integer2 is: {:p}", &integer2);
 }
